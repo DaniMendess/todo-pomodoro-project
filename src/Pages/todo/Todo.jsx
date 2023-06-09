062005d1e08f7fef43389a220a7ae2310da36fa9
DaniMendess <danimendes9728@gmail.com>
2023-06-08 23:18:06 -0300

    feat, task edit button

--- src/Pages/todo/Todo.jsx
+++ src/Pages/todo/Todo.jsx
@@ -7,63 +7,81 @@ import { BsPencil, BsCheck2, BsTrash } from "react-icons/bs";
 
 import Footer from "../../components/Footer"
 
+
+
 const Todo = () => {
-    const [tasks, setTask] = useState([])
-
-    const [complete, setComplete] = useState(true)
-
-    
-    // [{ id: random, task: "ISSO" },{ id: random, task: "ISSO" }]
-
-    const taskName = useRef()
-
-    // const newTask = {id:random, task: taskName.current.value}
+    const [tasks, setTasks] = useState([]);
+    const [modal, setModal] = useState('modal-wraper');
+    const taskName = useRef();
+    const inputEditTask = useRef();
+    const editTaskId = useRef(null);
 
     const handleItem = () => {
-        if(taskName.current.value) {
+        if (taskName.current.value) {
             document.querySelector('.btn__add__task').style.cursor = 'pointer'
             console.log("red")
-        }else{
+        } else {
             document.querySelector('.btn__add__task').style.cursor = 'no-drop'
         }
     }
 
-
-
     const addTask = () => {
-
-        if(!taskName.current.value ) {
+        if (!taskName.current.value) {
             handleItem()
             return
         }
 
-        const newTask = { id: uuidv4(), text: taskName.current.value, isComplete: false } 
-        setTask([...tasks, newTask])
+        const newTask = { id: uuidv4(), text: taskName.current.value, isComplete: false }
+        setTasks([...tasks, newTask])
     }
 
     const deleteTask = (taskId) => {
-        const newTask = tasks.filter(task => task.id !== taskId)
-        setTask(newTask)
+        const newTasks = tasks.filter(task => task.id !== taskId)
+        setTasks(newTasks)
     }
 
-    const isTaskComplete = (id) => {
+    const toggleTaskComplete = (id) => {
         const updatedTasks = tasks.map(task => {
-          if (task.id === id) {
-            return { ...task, isComplete: !task.isComplete };
-          }
-          return task;
+            if (task.id === id) {
+                return { ...task, isComplete: !task.isComplete };
+            }
+            return task;
         });
-      
-        setTask(updatedTasks);
-        console.log(updatedTasks);
-      };
+
+        setTasks(updatedTasks);
+    };
+
+    const editMyTask = (id) => {
+        if (modal === 'modal-wraper') {
+            setModal('modal-wraper open')
+        } else {
+            setModal('modal-wraper')
+        }
+        editTaskId.current = id;
+    }
+
+    const editTask = () => {
+        const id = editTaskId.current;
+        const nameTask = inputEditTask.current.value;
+        
+        const updatedTasks = tasks.map(task => {
+            if (task.id === id) {
+                return { ...task, text: nameTask };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+        setModal('modal-wraper');
+        inputEditTask.current.value = "";
+        editTaskId.current = null;
+    }
 
 
     return (
         <>
             <div className="all__items">
                 <div className="add__task">
-                    <input ref={taskName} className="task__item" type="text" placeholder="Adicione as suas tarefas..." onChange={handleItem}/>
+                    <input ref={taskName} className="task__item" type="text" placeholder="Adicione as suas tarefas..." onChange={handleItem} />
                     <button className="btn__add__task" onClick={addTask}>
                         <RiAddCircleLine size={25} />
                     </button>
@@ -71,42 +89,42 @@ const Todo = () => {
                 <main className="is__main">
                     {tasks.length > 0 && (
                         <div className="nav__search">
-                        <div className="search__list">
-                            <form className="search__list__item">
-                                <input className="search__item" placeholder="Pesquisar" type="text" />
-                                <button className="btn__search__task">
-                                    <RiSearchLine />
-                                </button>
+                            <div className="search__list">
+                                <form className="search__list__item">
+                                    <input className="search__item" placeholder="Pesquisar" type="text" />
+                                    <button className="btn__search__task">
+                                        <RiSearchLine />
+                                    </button>
 
-                                <select defaultValue={'DEFAULT'} className="select__item__search">
-                                    <option value="DEFAULT" disabled>Filtrar</option>
-                                    <option value="feitas">Feitas</option>
-                                    <option value="feitas">A fazer</option>
-                                </select>
-                            </form>
+                                    <select defaultValue={'DEFAULT'} className="select__item__search">
+                                        <option value="DEFAULT" disabled>Filtrar</option>
+                                        <option value="feitas">Feitas</option>
+                                        <option value="feitas">A fazer</option>
+                                    </select>
+                                </form>
+                            </div>
                         </div>
-                    </div>
                     )}
-                    
+
                     <div className="my__tasks ">
                         {tasks.length ? (
                             <>
                                 <div className="tasks">
-                                {tasks.map((task) => (
-                                    <div className={task.isComplete ? 'task-container completed' : 'task-container'} key={task.id}
-                                    >
-                                        <p style={{fontSize: 14}}className="task__name">{task.text}</p>
-                                        <div className="option__items">
-                                            <span><BsPencil /></span>
-                                            <span><BsCheck2 onClick={() => isTaskComplete(task.id)} /></span>
-                                            <span><BsTrash onClick={() => deleteTask(task.id)} /></span>
+                                    {tasks.map((task) => (
+                                        <div className={task.isComplete ? 'task-container completed' : 'task-container'} key={task.id}
+                                        >
+                                            <p style={{ fontSize: 14 }} className="task__name">{task.text}</p>
+                                            <div className="option__items">
+                                                <span><BsPencil onClick={() => editMyTask(task.id)} /></span>
+                                                <span><BsCheck2 onClick={() => toggleTaskComplete(task.id)} /></span>
+                                                <span><BsTrash onClick={() => deleteTask(task.id)} /></span>
+                                            </div>
                                         </div>
-                                    </div>
-                                ))
-                                }
-                            </div>
+                                    ))
+                                    }
+                                </div>
                             </>
-                            
+
                         ) : <div className="info__item">
                             <div className="note__img">
                                 <TbNotesOff size={90} />
@@ -122,22 +140,25 @@ const Todo = () => {
                     </div>
                 </main>
             </div>
-            <div className="modal-wraper  ">
+            {tasks.map((task) => (
+                <div className={modal} key={task.id}>
                 <div className="modal card">
-                    <form className="input__edit" action="">
-                        <input className="edit__task" type="text" placeholder="Digite o novo nome..." />
-                        <button className="btn__edit">
+                    <div className="input__edit">
+                        <input ref={inputEditTask} className="edit__task" type="text" placeholder="Digite o novo nome..." />
+                        <button className="btn__edit" onClick={() => editTask(task.id)} >
                             <BsPencil />
                         </button>
-                    </form>
+                    </div>
                     <h2>
-                        <button className="close" >
+                        <button className="close" onClick={editMyTask} >
                             <RiCloseFill />
                         </button>
                     </h2>
                 </div>
             </div>
-            <Footer/>
+            ))}
+            
+            <Footer />
         </>
     )
 }
