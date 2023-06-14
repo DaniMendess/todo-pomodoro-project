a292ff87219ecd64983da2a502bc5a1b64c903e4
DaniMendess <danimendes9728@gmail.com>
2023-06-13 21:15:10 -0300

    feat, new page

--- src/Pages/pomodoro/Pomodoro.jsx
+++ src/Pages/pomodoro/Pomodoro.jsx
@@ -1,31 +1,70 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Palm from "../../assets/HandPalm.svg"
 
 import { VscDebugStart } from "react-icons/vsc"
-
-
 import "./pomodoro.css"
 
 
+
+
 const Pomodoro = () => {
+    const inputMyTime = useRef()
+
+    const [erro, setErro] = useState(false)
+    const [isCount, setIsCount] = useState(false)
+    const [timeLeft, setTimeLeft] = useState(0);
+
+    const minutes = Math.floor(timeLeft / 60).toString().padStart(2, '0');
+    const seconds = (timeLeft - minutes * 60).toString().padStart(2, '0');
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            isCount && setTimeLeft((timeLeft) => timeLeft >= 1 ? timeLeft - 1 : 0)
+        }, 1000)
+        return () => {
+            clearInterval(interval)
+        }
+    }, [isCount])
+
+    const handleStart = () => {
+        const time = inputMyTime.current.value
+        if (time <= 0) {
+            setErro(true)
+            minutes = 0
+        }
+        const minutes = parseInt(inputMyTime.current.value);
+        setTimeLeft(minutes * 60);
+        setIsCount(true)
+        inputMyTime.current.value = ""
+        setErro(false)
+    }
+
+    const handleRestart = () => {
+        setIsCount(false)
+        setTimeLeft(2 * 60)
+    }
+
     return (
         <div className="container">
             <div className="info__time">
-                <input className="input__time" type="text" placeholder="Digite o tempo desejado" />
-                <button className="btn__start"><VscDebugStart size={30} /></button>
+                {erro ? <input className="input__time__erro" type="number" placeholder="Campo vazio" ref={inputMyTime} /> : <input className="input__time" type="number" placeholder="Digite o tempo desejado" ref={inputMyTime} />}
+                <button className="btn__start" onClick={handleStart} ><VscDebugStart size={30} /></button>
             </div>
 
             <main className="display__timer">
-                <div class="timer">
-                    <span className="minutes">10</span>
+                <div className="timer">
+                    <span className="minutes" >{minutes}</span>
                     <span className="ball">:</span>
-                    <span className="seconds">00</span>
+                    <span className="seconds">{seconds}</span>
                 </div>
-                <button class="controls">
-                
+                <button className="controls" onClick={handleRestart} >
+                    <img src={Palm} alt="palma-de-mão" />
                     PARAR
                 </button>
             </main>
         </div>
+        
     )
 }
 
